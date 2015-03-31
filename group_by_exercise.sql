be87313e21d736691524868a7ce5627a5028b9ad
Jamie Haskell <jmehaskell@gmail.com>
2015-03-31 11:57:08 -0500

    distinct and group by clauses added

---
+++ group_by_exercise.sql
@@ -0,0 +1,18 @@
+-- UNIQUE titles ordered alphabetically
+SELECT DISTINCT title FROM titles
+ORDER BY title;
+
+-- UNIQUE last names that start and end with 'E'
+SELECT last_name FROM employees WHERE last_name LIKE 'E%' 
+AND last_name LIKE '%e'
+GROUP BY last_name;
+
+-- unique combinations of first and last name where the last name starts and ends with 'e'
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' 
+AND last_name LIKE '%e'
+GROUP BY first_name, last_name;
+
+-- unique last names with a 'q' but not 'qu'
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' 
+AND last_name NOT LIKE '%qu%';
+
